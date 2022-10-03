9a4fde2665da17b8b80c7a9371736d73f5b223fe
Krotov Petr <83280920+KrotovPetr@users.noreply.github.com>
2022-10-03 14:44:29 +0300

    Make some fixes

--- converter/src/App.test.js
+++ converter/src/App.test.js
@@ -1,19 +1,23 @@
 import {getAnswer} from "./answer";
+describe("test",()=>{
+        test('My first test', () => {
+            expect(Math.max(1, 5, 10)).toBe(10);
+        });
 
-test('My first test', () => {
-    expect(Math.max(1, 5, 10)).toBe(10);
-});
+        test('Test correct data from cm to cm', () => {
+            expect(getAnswer(1,1,1)).toBe(1);
+        });
 
-test('Test correct data from cm to cm', () => {
-    expect(getAnswer(1,1,1)).toBe(1);
-});
+        test('Test correct data from cm to cm', () => {
+            expect(getAnswer(1,1,1)).toBe(1);
+        });
+        test('Test correct data from km to cm', () => {
+            expect(getAnswer(100000,1,1)).toBe(100000);
+        });
+        test('Test correct data from cm to inch', () => {
+            expect(getAnswer(1,2.54,10)).toBe(3.9370078740157477);
+        });
 
-test('Test correct data from cm to cm', () => {
-    expect(getAnswer(1,1,1)).toBe(1);
-});
-test('Test correct data from km to cm', () => {
-    expect(getAnswer(100000,1,1)).toBe(100000);
-});
-test('Test correct data from cm to inch', () => {
-    expect(getAnswer(1,2.54,10)).toBe(3.9370078740157477);
-});
+    }
+
+)
