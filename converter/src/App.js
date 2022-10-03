fe5e3bcbb216e1ec5e3b371b344ee879c413cf44
Krotov Petr <83280920+KrotovPetr@users.noreply.github.com>
2022-10-03 14:03:07 +0300

    Make radio more realistic

--- converter/src/App.js
+++ converter/src/App.js
@@ -5,15 +5,17 @@ function App() {
     const [value1, setValue1] = useState(null);
     const [value2, setValue2] = useState(null);
     const [value3, setValue3] = useState(1);
-    const clicker = (e)=>{
-        e.preventDefault();
-        e.stopPropagation();
-        console.log(e);
-    }
+
   return (
     <div className="App">
         <h1>Конвертер величин</h1>
-        <input type={"text"} placeholder={"Введите количество"} id={"inputValue"}/>
+        <input type={"text"} placeholder={"Введите количество"} id={"inputValue"} onChange={(e)=>{
+            if(!isNaN(+e.target.value)){
+                setValue3(+(e.target.value))
+            } else {
+                setValue1(1)
+            }
+        }}/>
         <div className={"translators"}>
             <form onSubmit={(e)=>{
                 e.preventDefault();
@@ -70,8 +72,6 @@ function App() {
                     }}/>
                     <label htmlFor="louie">Дюйм</label>
                 </div>
-                <button onSubmit={(e)=>{
-                }}>Выбрать</button>
             </form>
             <form onSubmit={(e)=>{
                 e.preventDefault();
@@ -85,55 +85,53 @@ function App() {
                 </div>
 
                 <div>
-                    <input type="radio" id="Километр" name="value" value="Километр" onChange={(e)=>{
-                        setValue2(1 / 100000 );
+                    <input type="radio" id="Километр" name="value" value="Километр"  onChange={(e)=>{
+                        setValue2(100000);
                     }}/>
                     <label htmlFor="Километр">Километр</label>
                 </div>
 
                 <div>
-                    <input type="radio" id="louie" name="value" value="Аршин" onChange={(e)=>{
-                        setValue2( 0.014061);
+                    <input type="radio" id="louie" name="value" value="Аршин"  onChange={(e)=>{
+                        setValue2(71.12);
                     }}/>
                     <label htmlFor="louie">Аршин</label>
                 </div>
                 <div>
-                    <input type="radio" id="louie" name="value" value="Вершина" onChange={(e)=>{
-                        setValue2( 0.224972);
+                    <input type="radio" id="louie" name="value" value="Вершина"  onChange={(e)=>{
+                        setValue2(4.45);
                     }}/>
                     <label htmlFor="louie">Вершок</label>
                 </div>
                 <div>
-                    <input type="radio" id="louie" name="value" value="Верста" onChange={(e)=>{
-                        setValue2(0.0000094);
+                    <input type="radio" id="louie" name="value" value="Верста"  onChange={(e)=>{
+                        setValue2(106680);
                     }}/>
                     <label htmlFor="louie">Верста</label>
                 </div>
                 <div>
-                    <input type="radio" id="louie" name="value" value="Миля" onChange={(e)=>{
-                        setValue2( 0.0000062);
+                    <input type="radio" id="louie" name="value" value="Миля"  onChange={(e)=>{
+                        setValue2(160934.4);
                     }}/>
                     <label htmlFor="louie">Миля</label>
                 </div>
                 <div>
-                    <input type="radio" id="louie" name="value" value="Фут" onChange={(e)=>{
-                        setValue2( 0.032808);
+                    <input type="radio" id="louie" name="value" value="Фут"  onChange={(e)=>{
+                        setValue2(30.48);
                     }}/>
                     <label htmlFor="louie">Фут</label>
                 </div>
                 <div>
-                    <input type="radio" id="louie" name="value" value="Дюйм" onChange={(e)=>{
-                        setValue2( 0.3937);
+                    <input type="radio" id="louie" name="value" value="Дюйм"  onChange={(e)=>{
+                        setValue2(2.54);
                     }}/>
                     <label htmlFor="louie">Дюйм</label>
                 </div>
-                <button onSubmit={(e)=>{
-                    console.log(2)
-                }}>Выбрать</button>
+
             </form>
 
         </div>
-        <h1>{value2}</h1>
+        <h1>Итог: {(value1/value2*(+value3))}</h1>
     </div>
   );
 }
