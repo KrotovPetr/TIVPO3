2a580de6e07d1934b2e758da8bd1d52abded4c20
Krotov Petr <83280920+KrotovPetr@users.noreply.github.com>
2022-10-24 11:41:46 +0300

    Add linters

--- converter/src/App.test.js
+++ converter/src/App.test.js
@@ -4,17 +4,17 @@ describe("test",()=>{
             expect(Math.max(1, 5, 10)).toBe(10);
         });
 
-        test('Test correct data from cm to cm', () => {
+        test('correct data from cm to cm', () => {
             expect(getAnswer(1,1,1)).toBe(1);
         });
 
-        test('Test correct data from cm to cm', () => {
+        test('correct data from cm to cm', () => {
             expect(getAnswer(1,1,1)).toBe(1);
         });
-        test('Test correct data from km to cm', () => {
+        test('correct data from km to cm', () => {
             expect(getAnswer(100000,1,1)).toBe(100000);
         });
-        test('Test correct data from cm to inch', () => {
+        test('correct data from cm to inch', () => {
             expect(getAnswer(1,2.54,10)).toBe(3.9370078740157477);
         });
 
